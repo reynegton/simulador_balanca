10c019bd70ccbe7209a9b5be52c33027ff989549
Reynegton Pinheiro Nunes <reynegton@gmail.com>
2022-09-20 14:39:19 -0300

    show banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:simulador_balanca/home/UI/home_screen.dart';
 
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: kDebugMode,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
