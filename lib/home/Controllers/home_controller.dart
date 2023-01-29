2db1a3e7c96b9085746749de4c2be6ff0dbee491
Reynegton Pinheiro Nunes <reynegton@gmail.com>
2023-01-29 11:55:06 -0300

    melhorias e update versao flutter

--- lib/home/Controllers/home_controller.dart
+++ lib/home/Controllers/home_controller.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import '../../Utils/shared_preferences_helper.dart';
+
 class HomeController {
   ValueNotifier<int> minMaxValue = ValueNotifier(999999);
   ValueNotifier<int> casasDecimais = ValueNotifier(1);
@@ -9,6 +11,25 @@ class HomeController {
   ValueNotifier<bool> oscilarPeso = ValueNotifier(false);
   ValueNotifier<int> pesoOscilacao = ValueNotifier(0);
   ValueNotifier<int> taraTela = ValueNotifier(0);
+
+  final TextEditingController textControllerPorta;
+  final TextEditingController textControllerTara;
+  final TextEditingController textControllerPesoOscilacao;
+  final TextEditingController textControllerMaxMinValue;
+  final TextEditingController textControllerCasasDecimais;
+  final TextEditingController textControllerPeso;
+
+  HomeController({
+    required this.textControllerPorta,
+    required this.textControllerCasasDecimais,
+    required this.textControllerMaxMinValue,
+    required this.textControllerPeso,
+    required this.textControllerPesoOscilacao,
+    required this.textControllerTara,
+  }) {
+    _addListeners();
+    _loadPreferencesValue();
+  }
 
   void incrementarPeso(int value) {
     pesoTela.value += value;
@@ -39,6 +60,7 @@ class HomeController {
     }
     minMaxValue.value = minMax;
   }
+
   void setMinMaxIntValue(int value) {
     var minMax = value;
     minMaxValue.value = minMax;
@@ -67,5 +89,61 @@ class HomeController {
             .replaceAll('.', '')
             .replaceAll(',', '')) ??
         0;
+  }
+
+  void _addListeners() {
+    textControllerTara.addListener(
+      () {
+        setTaraTelaValue(textControllerTara.text);
+      },
+    );
+    textControllerPesoOscilacao.addListener(
+      () {
+        setPesoOscilacao(textControllerPesoOscilacao.text);
+      },
+    );
+    textControllerMaxMinValue.addListener(
+      () {
+        setMinMaxValue(textControllerMaxMinValue.text);
+      },
+    );
+    textControllerCasasDecimais.addListener(
+      () {
+        setCasasDecimais(textControllerCasasDecimais.text);
+      },
+    );
+
+    casasDecimais.addListener(
+      () {
+        SharedPreferencesHelper.instance.saveInt(
+            EnumKeysSharedPreferences.eCasasDecimais, casasDecimais.value);
+
+        textControllerMaxMinValue.text = getValueDivisor(minMaxValue.value);
+        textControllerTara.text = getValueDivisor(taraTela.value);
+        textControllerPesoOscilacao.text = getValueDivisor(pesoOscilacao.value);
+      },
+    );
+    minMaxValue.addListener(
+      () {
+        SharedPreferencesHelper.instance
+            .saveInt(EnumKeysSharedPreferences.ePesoMinMax, minMaxValue.value);
+      },
+    );
+  }
+
+  Future<void> _loadPreferencesValue() async {
+    var casasDecimaisAux = await SharedPreferencesHelper.instance
+        .loadInt(EnumKeysSharedPreferences.eCasasDecimais);
+    if (casasDecimaisAux != null) {
+      setCasasDecimais(casasDecimaisAux.toString());
+      textControllerCasasDecimais.text = casasDecimaisAux.toString();
+    }
+
+    var minMaxAux = await SharedPreferencesHelper.instance
+        .loadInt(EnumKeysSharedPreferences.ePesoMinMax);
+    if (minMaxAux != null) {
+      setMinMaxIntValue(minMaxAux);
+      textControllerMaxMinValue.text = getValueDivisor(minMaxAux);
+    }
   }
 }
